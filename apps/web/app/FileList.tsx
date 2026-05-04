24a0ff8ab7d43ad3b57681f257b16eb50624d928
djwname <djwname@gmail.com>
2026-05-04 11:56:58 +0800

    release: 1.0.14

--- apps/web/app/FileList.tsx
+++ apps/web/app/FileList.tsx
@@ -218,7 +218,7 @@ Size: ${formatBytes(file.size)}</pre>
         lang="en"
         toolbarStyle="default"
         fontSize="sm"
-        theme="dark"
+        theme="auto"
         features={{
           uploadFile: true,
           uploadFolder: true,
